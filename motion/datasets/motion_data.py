b062531dcca27471903a453cfde9eae93fd399d6
nagyrajmund <rajmund977@gmail.com>
2020-12-04 01:17:29 +0100

    Rework dataset to avoid memory issues with text
    The original implementation loads the entire conditioning info to memory, which is not feasible with 773-dimensional BERT embeddings

--- motion/datasets/motion_data.py
+++ motion/datasets/motion_data.py
@@ -6,51 +6,70 @@ class MotionDataset(Dataset):
     Motion dataset. 
     Prepares conditioning information (previous poses + control signal) and the corresponding next poses"""
 
-    def __init__(self, control_data, joint_data, seqlen, n_lookahead, dropout):
+    def __init__(self, control_data, joint_data, past_context_len, future_context_len, dropout):
         """
         Args:
         control_data: The control input
         joint_data: body pose input 
         Both with shape (samples, time-slices, features)
-        seqlen: number of autoregressive body poses and previous control values
-        n_lookahead: number of future control-values
+        past_context_len: number of autoregressive body poses and previous control values
+        future_context_len: number of future control-values
         dropout: (0-1) dropout probability for previous poses
         """
-        self.seqlen = seqlen
-        self.dropout=dropout
-        seqlen_control = seqlen + n_lookahead + 1
-        
+        self.control_data = control_data
+        self.joint_data = joint_data
+        self.past_context_len = past_context_len
+        self.future_context_len = future_context_len
+        self.dropout = dropout
+        seqlen_control = past_context_len + future_context_len + 1
+        self.control_seqlen = seqlen_control
         #For LSTM network
         n_frames = joint_data.shape[1]
                     
         # Joint positions for n previous frames
-        autoreg = self.concat_sequence(self.seqlen, joint_data[:,:n_frames-n_lookahead-1,:])
+        ## autoreg = self.concat_sequence(self.past_context_len, joint_data[:,:n_frames-future_context_len-1,:])
                     
         # Control for n previous frames + current frame
-        control = self.concat_sequence(seqlen_control, control_data)
+        ## control = self.concat_sequence(seqlen_control, control_data)
 
-        # conditioning
-        
-        print("autoreg:" + str(autoreg.shape))        
-        print("control:" + str(control.shape))        
-        new_cond = np.concatenate((autoreg,control),axis=2)
 
-        # joint positions for the current frame
-        x_start = seqlen
-        new_x = self.concat_sequence(1, joint_data[:,x_start:n_frames-n_lookahead,:])
-        self.x = new_x
-        self.cond = new_cond
+        ## print("autoreg:" + str(autoreg.shape))        
+        ## print("control:" + str(control.shape))        
+        ## new_cond = np.concatenate((autoreg,control),axis=2)
+
+        # Joint positions for the current frame
+        self.x = joint_data[:, past_context_len:n_frames-future_context_len, :]
+        ## self.cond = new_cond
         
         #TODO TEMP swap C and T axis to match existing implementation
         self.x = np.swapaxes(self.x, 1, 2)
-        self.cond = np.swapaxes(self.cond, 1, 2)
+        ## self.cond = np.swapaxes(self.cond, 1, 2)
         
         print("self.x:" + str(self.x.shape))        
-        print("self.cond:" + str(self.cond.shape))
-        
+        ## print("self.cond:" + str(self.cond.shape))
+        self.cond_dim = self.get_conditioning(0).shape[0]
+
     def n_channels(self):
-        return self.x.shape[1], self.cond.shape[1]
-		
+        return self.x.shape[1], self.cond_dim
+
+    def get_conditioning(self, batch_idx):
+        n_frames = self.joint_data.shape[1]
+        
+        autoreg = self.concat_sequence(
+            seqlen = self.past_context_len, 
+            data = self.joint_data[batch_idx,:n_frames-self.future_context_len-1,:][np.newaxis, :]
+        )
+
+        control = self.concat_sequence(
+            seqlen = self.control_seqlen,
+            data = self.control_data[batch_idx][np.newaxis, :]
+        )
+
+        conditioning = np.concatenate((autoreg, control), axis=2)
+        #TODO TEMP swap C and T axis to match existing implementation
+        conditioning = np.swapaxes(conditioning, 1, 2)
+        return conditioning.squeeze(axis=0)
+
     def concat_sequence(self, seqlen, data):
         """ 
         Concatenates a sequence of features to one.
@@ -86,12 +105,11 @@ class MotionDataset(Dataset):
         
         if self.dropout>0.:
             n_feats, tt = self.x[idx,:,:].shape
-            cond_masked = self.cond[idx,:,:].copy()
-            
-            keep_pose = np.random.rand(self.seqlen, tt)<(1-self.dropout)
+            cond_masked = self.get_conditioning(idx)
+            keep_pose = np.random.rand(self.past_context_len, tt)<(1-self.dropout)
 
             #print(keep_pose)
-            n_cond = cond_masked.shape[0]-(n_feats*self.seqlen)
+            n_cond = cond_masked.shape[0]-(n_feats*self.past_context_len)
             mask_cond = np.full((n_cond, tt), True)
 
             mask = np.repeat(keep_pose, n_feats, axis = 0)
